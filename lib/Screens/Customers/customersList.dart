5605f2f5fc453cfcede483e41f0609d6bd6697de
ShowmikDebnath <67783022+ShowmikDebnath@users.noreply.github.com>
2024-02-22 13:29:21 +0600

    customer search option added

--- lib/Screens/Customers/customersList.dart
+++ lib/Screens/Customers/customersList.dart
@@ -13,6 +13,22 @@ class CustomerList extends StatefulWidget {
 }
 
 class _CustomerListState extends State<CustomerList> {
+  late TextEditingController _searchController;
+  late Stream<QuerySnapshot> _customerStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController();
+    _customerStream = FirebaseFirestore.instance.collection('customers').snapshots();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     double _height = MediaQuery.of(context).size.height;
@@ -26,25 +42,28 @@ class _CustomerListState extends State<CustomerList> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            MediaQuery.of(context).size.width >915? Image.asset(
+            MediaQuery.of(context).size.width > 915
+                ? Image.asset(
               'assets/logo.jpg',
               width: 400,
               fit: BoxFit.contain,
               height: 160,
             )
-                : MediaQuery.of(context).size.width >852? Image.asset(
+                : MediaQuery.of(context).size.width > 852
+                ? Image.asset(
               'assets/logo.jpg',
-              width: _width/2.5,
+              width: _width / 2.5,
               fit: BoxFit.contain,
-              height:160,
-            ): Image.asset(
+              height: 160,
+            )
+                : Image.asset(
               'assets/logo.jpg',
-              width: _width/4,
+              width: _width / 4,
               fit: BoxFit.contain,
-              height:160,
+              height: 160,
             ),
-
-            MediaQuery.of(context).size.width >671? Row(
+            MediaQuery.of(context).size.width > 671
+                ? Row(
               children: [
                 InkWell(
                   onTap: () {
@@ -57,7 +76,6 @@ class _CustomerListState extends State<CustomerList> {
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 16,
-
                       ),
                     ),
                   ),
@@ -78,8 +96,6 @@ class _CustomerListState extends State<CustomerList> {
                     ),
                   ),
                 ),
-
-
                 SizedBox(width: 10),
                 InkWell(
                   onTap: () {
@@ -130,7 +146,6 @@ class _CustomerListState extends State<CustomerList> {
                   ),
                 ),
                 SizedBox(width: 10),
-
               ],
             )
                 : Row(
@@ -146,7 +161,6 @@ class _CustomerListState extends State<CustomerList> {
                       style: TextStyle(
                         color: Colors.black,
                         fontSize: 12,
-
                       ),
                     ),
                   ),
@@ -167,8 +181,6 @@ class _CustomerListState extends State<CustomerList> {
                     ),
                   ),
                 ),
-
-
                 SizedBox(width: 5),
                 InkWell(
                   onTap: () {
@@ -219,7 +231,6 @@ class _CustomerListState extends State<CustomerList> {
                   ),
                 ),
                 SizedBox(width: 0),
-
               ],
             ),
           ],
@@ -232,9 +243,40 @@ class _CustomerListState extends State<CustomerList> {
           children: [
             Container(
               margin: EdgeInsets.only(right: _width / 6, left: _width / 6),
-              child: const Text(
-                "Customers List",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              child: Row(
+                children: [
+                  const Text(
+                    "Customers List",
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: _width / 4,),
+                  Expanded(
+                    child: TextFormField(
+
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        enabledBorder: const OutlineInputBorder(
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(5.0)),
+                          borderSide: BorderSide(color: Colors.transparent),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(5.0)),
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        hintText: "Search...",
+                        fillColor: Colors.grey[200],
+                        filled: true,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
             SizedBox(height: 20),
@@ -242,64 +284,82 @@ class _CustomerListState extends State<CustomerList> {
               child: Container(
                 margin: EdgeInsets.only(right: _width / 6, left: _width / 6),
                 child: StreamBuilder(
-                  stream: FirebaseFirestore.instance.collection('customers').snapshots(),
+                  stream: _customerStream,
                   builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
+                      return Center(child: CircularProgressIndicator());
                     } else {
                       if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
+                        return Center(child: Text('Error: ${snapshot.error}'));
                       } else {
-                        return ListView.separated(
-                          itemCount: snapshot.data!.docs.length,
-                          separatorBuilder: (context, index) => SizedBox(height: 10),
-                          itemBuilder: (context, index) {
-                            DocumentSnapshot customer = snapshot.data!.docs[index];
-                            return ListTile(
-                              title: Row(
-                                children: [
-                                  Icon(Icons.person, size: 16,),
-                                  SizedBox(width: 5,),
-                                  Text(
+                        if (snapshot.data!.docs.isEmpty) {
+                          return Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.warning, size: 48, color: Colors.grey),
+                                SizedBox(height: 10),
+                                Text('No customers available', style: TextStyle(fontSize: 16)),
+                              ],
+                            ),
+                          );
+                        } else {
+                          // Filtering logic based on search input
+                          var filteredCustomers = snapshot.data!.docs.where((customer) {
+                            String name = customer['name'].toString().toLowerCase();
+                            String searchTerm = _searchController.text.toLowerCase();
+                            return name.contains(searchTerm);
+                          }).toList();
+
+                          return ListView.separated(
+                            itemCount: filteredCustomers.length,
+                            separatorBuilder: (context, index) => SizedBox(height: 10),
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot customer = filteredCustomers[index];
+                              return ListTile(
+                                title: Row(
+                                  children: [
+                                    Icon(Icons.person, size: 16,),
+                                    SizedBox(width: 5,),
+                                    Text(
                                       customer['name'],
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
                                     ),
-                                  ),
-                                  Spacer(),
-                                  Text("Due: ${customer['due']}"),
-                                ],
-                              ),
-                              subtitle: Row(
-                                children: [
-                                  Icon(Icons.phone, size: 16,),
-                                  SizedBox(width: 5,),
-                                  Text(
+                                    Spacer(),
+                                    Text("Due: ${customer['due']}"),
+                                  ],
+                                ),
+                                subtitle: Row(
+                                  children: [
+                                    Icon(Icons.phone, size: 16,),
+                                    SizedBox(width: 5,),
+                                    Text(
                                       customer['phone'],
-                                    style: TextStyle(
-                                      color: Colors.grey,
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              onTap: () {
-                                Get.to(CustomerDetails(customer: customer));
-                              },
-                            );
-                          },
-                        );
+                                  ],
+                                ),
+                                onTap: () {
+                                  Get.to(CustomerDetails(customer: customer));
+                                },
+                              );
+                            },
+                          );
+                        }
                       }
                     }
                   },
                 ),
               ),
             ),
-
           ],
         ),
       ),
-
     );
   }
 }
